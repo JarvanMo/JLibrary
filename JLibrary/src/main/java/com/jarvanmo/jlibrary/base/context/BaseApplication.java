1d26d08fe38c1df7cdc03cb6319506304e892039
JarvanMo <jarvan.mo@gmail.com>
2016-03-29 14:52:57 +0800

    modify adapter

--- JLibrary/src/main/java/com/jarvanmo/jlibrary/base/context/BaseApplication.java
+++ JLibrary/src/main/java/com/jarvanmo/jlibrary/base/context/BaseApplication.java
@@ -2,21 +2,38 @@ package com.jarvanmo.jlibrary.base.context;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.jarvanmo.jlibrary.system.SystemManager;
 import com.jarvanmo.jlibrary.system.SystemSetting;
 import com.jarvanmo.jlibrary.widget.toast.JToast;
 
+import okhttp3.OkHttpClient;
+
 /**
  * Created by mo on 16-3-26.
- *
  */
-public class BaseApplication  extends Application {
+public class BaseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        initSystem();
+        initFresco();
+    }
+
+    private void initSystem() {
         JToast.init(this);
         SystemManager.getInstance();
         SystemSetting.SystemExitConfig.exitNames.add(SystemSetting.SystemExitConfig.defaultExitName);
     }
+
+
+    private void initFresco() {
+        Fresco.initialize(this);
+
+
+    }
+
 }
